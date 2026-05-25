0217847c36f91511881db0329957c31bdc8457db
Gopal_Nayak <gopal32452@gmail.com>
2026-05-25 10:19:38 +0530

    fixed mongo session store

--- server.js
+++ server.js
@@ -74,15 +74,15 @@ app.use(express.static(path.join(__dirname, "public")));
 const store = MongoStore.create({
   mongoUrl: dbUrl,
 
+  touchAfter: 24 * 3600,
+
   crypto: {
     secret: process.env.SESSION_SECRET,
   },
-
-  touchAfter: 24 * 3600,
 });
 
 store.on("error", (err) => {
-  console.log("❌ SESSION STORE ERROR", err);
+  console.log("❌ SESSION STORE ERROR:", err);
 });
 
 // ======================
